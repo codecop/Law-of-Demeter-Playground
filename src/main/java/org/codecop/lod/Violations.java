57cc6f99d0623cc12b49fc61d4943b06660d6964
Peter Kofler <peter.kofler@code-cop.org>
2023-11-25 22:01:18 +0100

    Try it for real: Build breaks on violation.

--- src/main/java/org/codecop/lod/Violations.java
+++ src/main/java/org/codecop/lod/Violations.java
@@ -6,8 +6,8 @@ public class Violations {
     void usesEnums() {
         Values one = Values.ONE;
         System.out.println(one);
-        System.out.println(one.name()); // 1 LoD violation "object not created locally"
-        System.out.println(one.name().length()); // 3 LoD violations
+        // System.out.println(one.name()); // 1 LoD violation "object not created locally"
+        // System.out.println(one.name().length()); // 3 LoD violations
     }
 
 }
