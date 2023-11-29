ff2f5b83197f68748442d9aebc6e7eb155855481
Peter Kofler <peter.kofler@code-cop.org>
2023-11-29 20:42:48 +0100

    Acceptance Test: Simple addition works.

--- src/test/java/kata/lisp/a20231125/MainTest.java
+++ src/test/java/kata/lisp/a20231125/MainTest.java
@@ -4,16 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
 
     @Test
-    @Disabled("Guiding Test")
+    // @Disabled("Guiding Test")
     void evaluateSingleFile() throws IOException {
         Result actual = new Main().run("src/test/resources/single_addition.scm");
-        assertEquals("5 (java.lang.Integer)", actual.toString());
+        assertEquals("5 (ResultType N of class java.lang.Integer)", actual.toString());
     }
 
 }
