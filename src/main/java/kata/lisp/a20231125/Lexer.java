914c2e748dfc8cb7a7348357c34403bc6398ded7
Peter Kofler <peter.kofler@code-cop.org>
2023-11-29 20:03:30 +0100

    Two tokens. LoD OK.

--- src/main/java/kata/lisp/a20231125/Lexer.java
+++ src/main/java/kata/lisp/a20231125/Lexer.java
@@ -1,10 +1,24 @@
 package kata.lisp.a20231125;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lexer {
 
     public Tokens tokenise(String code) {
-        Token token = new Token(code);
-        return new Tokens(token);
+        List<String> pieces = new ArrayList<>();
+
+        int lastStart = 0;
+        for (int current = 0; current < code.length(); current++) {
+            char ch = code.charAt(current);
+            if (Character.isWhitespace(ch)) {
+                pieces.add(code.substring(lastStart, current));
+                lastStart = current + 1;
+            }
+        }
+        pieces.add(code.substring(lastStart));
+
+        return Tokens.tokensOf(pieces.toArray(new String[0]));
     }
 
 }
--- src/test/java/kata/lisp/a20231125/LexerTest.java
+++ src/test/java/kata/lisp/a20231125/LexerTest.java
@@ -16,4 +16,14 @@ class LexerTest {
         assertEquals(new Token("1"), tokens.next());
     }
 
+    @Test
+    void twoTokensByBlank() {
+        Tokens tokens = lexer.tokenise("1 2");
+
+        assertEquals(2, tokens.size());
+        assertEquals(new Token("1"), tokens.next());
+        tokens.consumeToken();
+        assertEquals(new Token("2"), tokens.next());
+    }
+
 }
